3243ee1ab2489d7401cd20c7d880d9ec3bd8aaef
Justin Christian <jstnchristian@gmail.com>
2017-09-10 20:42:50 +0000

    First commit

---
+++ CreateThumbnail.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+import boto3
+import os
+import sys
+import uuid
+from PIL import Image
+import PIL.Image
+     
+s3_client = boto3.client('s3')
+     
+def resize_image(image_path, resized_path):
+    with Image.open(image_path) as image:
+        image.thumbnail(tuple(x / 2 for x in image.size))
+        image.save(resized_path)
+     
+def handler(event, context):
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        #make key equal to the last string
+        key = record['s3']['object']['key']
+        download_key=key.split('/')[-1]
+        download_path = '/tmp/{}{}'.format(uuid.uuid4(), download_key)
+        upload_path = '/tmp/{}'.format(download_key)
+        
+        s3_client.download_file(bucket, key, download_path)
+        resize_image(download_path, upload_path)
+        s3_client.upload_file(upload_path, '{}'.format(bucket), 'images/thumbs/{}'.format(download_key))
