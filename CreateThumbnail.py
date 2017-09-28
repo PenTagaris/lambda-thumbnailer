312c78811ca65bdd7b7dc2f8014923b1b053c781
EC2 Default User <jstnchristian@gmail.com>
2017-09-28 11:21:17 +0000

    Added the entire virtualenv for later re-creation, and to allow for testing.

--- CreateThumbnail.py
+++ CreateThumbnail.py
@@ -10,7 +10,7 @@ s3_client = boto3.client('s3')
      
 def resize_image(image_path, resized_path):
     with Image.open(image_path) as image:
-        image.thumbnail(tuple(x / 2 for x in image.size))
+        image.thumbnail(tuple(x / 4 for x in image.size))
         image.save(resized_path)
      
 def handler(event, context):
@@ -18,10 +18,16 @@ def handler(event, context):
         bucket = record['s3']['bucket']['name']
         #make key equal to the last string
         key = record['s3']['object']['key']
-        download_key=key.split('/')[-1]
+        download_key=key.split('/',2)[2].replace('/','__')
         download_path = '/tmp/{}{}'.format(uuid.uuid4(), download_key)
         upload_path = '/tmp/{}'.format(download_key)
-        
+
+        #Copy the image to the "hi_res" folder
+        #copy_source = {
+        #    'Bucket': bucket,
+        #    'Key': key
+        #}
+        #s3.meta.client.copy(copy_source, bucket, 'hi_res/{}'.format(download_key)) 
         s3_client.download_file(bucket, key, download_path)
         resize_image(download_path, upload_path)
-        s3_client.upload_file(upload_path, '{}'.format(bucket), 'images/thumbs/{}'.format(download_key))
+        s3_client.upload_file(upload_path, '{}'.format(bucket), 'images/thumbs/{}'.format(download_key.replace('__','/')))
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # lambda-thumbnailer
-My edited lambda thumbnailer
+My edited lambda thumbnailer. To create the zip file required by Lambda, do the following:
+
+```
+$ source lambda-thumbnailer/bin/activate
+$ zip -r9 ~/CreateThumbnail.zip *
+$ zip -g CreateThumbnail.zip CreateThumbnail.zip
+```
+
