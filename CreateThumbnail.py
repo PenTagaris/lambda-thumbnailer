e656552b13ac5f8bc80f60d97e8ac380f8ae9b0e
EC2 Default User <jstnchristian@gmail.com>
2017-11-13 17:35:22 +0000

    First go at imageops-fit

--- CreateThumbnail.py
+++ CreateThumbnail.py
@@ -3,21 +3,17 @@ import boto3
 import os
 import sys
 import uuid
-from PIL import Image
-import PIL.Image
+from PIL import Image, ImageOps
      
 s3_client = boto3.client('s3')
      
-def resize_image(image_path, resized_path, size):
+def resize_image(image_path, resized_path, new_size):
     with Image.open(image_path) as image:
-        resized_path = ImageOps.fit(
-            image,
-            (size, size),
-            Image.ANTIALIAS
-        )
-        #image.thumbnail(tuple(x / 4 for x in image.size))
-        #image.save(resized_path)
-     
+        dimg = ImageOps.fit(image,
+            size=(new_size, new_size),
+            centering=(0.5, 0.5))
+        dimg.save(resized_path)
+
 def handler(event, context):
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
@@ -34,5 +30,6 @@ def handler(event, context):
         #}
         #s3.meta.client.copy(copy_source, bucket, 'hi_res/{}'.format(download_key)) 
         s3_client.download_file(bucket, key, download_path)
-        resize_image(download_path, upload_path, 1024)
-        s3_client.upload_file(upload_path, '{}'.format(bucket), 'images/thumbs/{}'.format(download_key.replace('__','/')))
+        for i in [240, 480, 960]:
+            resize_image(download_path, upload_path, i)
+            s3_client.upload_file(upload_path, '{}'.format(bucket), 'images/{}/{}'.format(i, download_key.replace('__','/')))
